c0d69f3a2446508310f631d0d0c1e3da55457705
Marco Rauscher <marco.mr.rauscher@gmail.com>
2018-04-11 09:05:20 +0200

    [MR, MF] Add deleteLecture functionality, additional tests

--- app/src/androidTest/java/com/intelligent/morning06/lecturemate/DataBaseAccessLectureInstrumentedTest.java
+++ app/src/androidTest/java/com/intelligent/morning06/lecturemate/DataBaseAccessLectureInstrumentedTest.java
@@ -2,6 +2,7 @@ package com.intelligent.morning06.lecturemate;
 
 import com.intelligent.morning06.lecturemate.DataAccess.DataBaseAccessLecture;
 import com.intelligent.morning06.lecturemate.DataAccess.Exceptions.LectureAlreadyExistsException;
+import com.intelligent.morning06.lecturemate.DataAccess.Exceptions.LectureDoesNotExistException;
 import com.intelligent.morning06.lecturemate.DataAccess.Lecture;
 
 import android.support.test.InstrumentationRegistry;
@@ -31,7 +32,7 @@ public class DataBaseAccessLectureInstrumentedTest {
         if(dataBase == null)
             dataBase = new DataBaseAccessLecture(InstrumentationRegistry.getTargetContext());
 
-        dataBase.deleteAllLectures();
+        dataBase.DeleteAllLectures();
     }
 
     @Test
@@ -43,7 +44,7 @@ public class DataBaseAccessLectureInstrumentedTest {
         assertFalse(lecture.getId() == -1);
         assertNotNull(lectures);
         assertEquals(1, lectures.size());
-        assertEquals(lectures.get(0).getLectureName(), "Softwaretechnologie");
+        assertEquals("Softwaretechnologie", lectures.get(0).getLectureName());
     }
 
     @Test
@@ -60,11 +61,63 @@ public class DataBaseAccessLectureInstrumentedTest {
     }
 
     public void addLecture_LectureNameEmpty_throwsIllegalArgumentException() throws Exception {
-
+        try {
+            dataBase.AddLecture("");
+            Assert.fail("Should have thrown IllegalArgumentException.");
+        }
+        catch(IllegalArgumentException exception) {
+            assertEquals("lectureName", exception.getMessage());
+        }
     }
 
     @Test
     public void deleteLecture_LectureExists_isCorrectlyDeleted() throws Exception {
+        dataBase.AddLecture("TestLecture");
+        dataBase.DeleteLecture("TestLecture");
 
+        List<Lecture> lectures = dataBase.GetAllLectures();
+        assertEquals(0, lectures.size());
+    }
+
+    @Test
+    public void deleteLecture_LectureDoesNotExist_throwsLectureDoesNotExistException() throws Exception {
+        try {
+            dataBase.DeleteLecture("TestLecture");
+            Assert.fail("Should have thrown LectureDoesNotExistException");
+        } catch(LectureDoesNotExistException exception) {
+            assertEquals("TestLecture", exception.getLectureName());
+        }
+    }
+
+    @Test
+    public void deleteLecture_LectureNameEmpty_throwsIllegalArgumentException() throws Exception {
+        try {
+            dataBase.DeleteLecture("");
+            Assert.fail("Should have thrown IllegalArgumentException");
+        } catch(IllegalArgumentException exception) {
+            assertEquals("lectureName", exception.getMessage());
+        }
+    }
+
+    @Test
+    public void getAllLectures_noLecturesAvailable_returnsEmptyList() throws Exception {
+        List<Lecture> lectures = dataBase.GetAllLectures();
+        assertEquals(0, lectures.size());
+    }
+
+    @Test
+    public void getAllLectures_lecturesAvailable_returnsListWithAllLectures() throws Exception {
+        dataBase.AddLecture("first");
+        dataBase.AddLecture("second");
+        dataBase.AddLecture("third");
+        dataBase.AddLecture("fourth");
+
+        List<Lecture> lectures = dataBase.GetAllLectures();
+
+        assertEquals(4, lectures.size());
+        assertEquals("first", lectures.get(0).getLectureName());
+        assertEquals("second", lectures.get(1).getLectureName());
+        assertEquals("third", lectures.get(2).getLectureName());
+        assertEquals("fourth", lectures.get(3).getLectureName());
     }
 }
