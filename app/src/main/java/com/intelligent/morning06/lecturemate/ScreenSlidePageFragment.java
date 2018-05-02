a4bfb6cc981e653fd8465db8d99d53f022652892
commonnickname <i.televan@student.tugraz.at>
2018-05-02 11:42:59 +0200

    [IT, FR] - Framework for image viewing with swiping

---
+++ app/src/main/java/com/intelligent/morning06/lecturemate/ScreenSlidePageFragment.java
@@ -0,0 +1,23 @@
+package com.intelligent.morning06.lecturemate;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.intelligent.morning06.lecturemate.R;
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.activity_image_view, container, false);
+
+        return rootView;
+    }
+}
